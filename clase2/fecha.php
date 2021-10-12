0b80b7c6441afe4b5c0a22b7893b8ee4cf60c550
Marcos <exegeses@gmail.com>
2021-10-12 11:52:45 -0300

    Material clase 2

--- clase2/fecha.php
+++ clase2/fecha.php
@@ -1,1 +1,16 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Trabajo con la fecha en PHP</title>
+</head>
+<body>
+    <h1>Trabajo con la fecha en PHP</h1>
+
+
+
+
+</body>
+</html>
