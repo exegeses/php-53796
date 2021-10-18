6fad5db0f3dc6949b198b21cb9c004f40fbde85b
Marcos <exegeses@gmail.com>
2021-10-18 10:36:28 -0300

    Práctica bucles v1 sin terminar

---
+++ clase4/locaciones.php
@@ -0,0 +1,36 @@
+<?php
+    $locaciones =
+        [
+            'angkor', 'azul', 'basil', 'burj',
+            'colosseo', 'easter', 'eiffel',
+            'gizah', 'ha-long', 'liberty',
+            'machu', 'opera', 'palace', 'petra',
+            'sagrada', 'santorini', 'taj',
+            'wall'
+        ];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Locaciones</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <main>
+
+
+        <article>
+            <img src="locaciones/acropolis.jpg">
+            <h2>Nombre</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto, deserunt exercitationem harum impedit labore mollitia obcaecati provident sit tempore?
+            </p>
+        </article>
+
+
+    </main>
+</body>
+</html>
