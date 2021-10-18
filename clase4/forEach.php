1c03dfc269ab342bea7d0ca145231ae6c08171ad
Marcos <exegeses@gmail.com>
2021-10-18 11:18:04 -0300

    Explicación bucle foreach con valor

---
+++ clase4/forEach.php
@@ -0,0 +1,12 @@
+<?php
+
+    $autos = [
+        'Aston Matin', 'Pagani', 'Bugatti',
+        'Maseratti', 'Lotus', 'Ferrari',
+        'Porsche', 'Tesla'
+    ];
+
+    //foreach ( $coleccion as $auxiliar )
+    foreach ( $autos as $auto ){
+        echo $auto, '<br>';
+    }
