5d365f9f0b828d4e4313b405cab50c5a92424333
Marcos <exegeses@gmail.com>
2021-10-18 11:22:36 -0300

    Explicación bucle foreach con clave=>valor

--- clase4/forEach.php
+++ clase4/forEach.php
@@ -1,12 +1,17 @@
 <?php
 
     $autos = [
-        'Aston Matin', 'Pagani', 'Bugatti',
+        'Aston Martin', 'Pagani', 'Bugatti',
         'Maseratti', 'Lotus', 'Ferrari',
         'Porsche', 'Tesla'
     ];
 
     //foreach ( $coleccion as $auxiliar )
-    foreach ( $autos as $auto ){
+    /*foreach ( $autos as $auto ){
         echo $auto, '<br>';
+    }*/
+
+    //foreach ( $coleccion as $clave=>$valor  )
+    foreach ( $autos as $key=>$auto ){
+        echo $key, ': ', $auto, '<br>';
     }
