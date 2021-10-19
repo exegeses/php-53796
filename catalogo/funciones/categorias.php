e8ff47526598ba4a60ba71e34047efa6e320bdba
Marcos <exegeses@gmail.com>
2021-10-19 13:11:07 -0300

    panel de administración de marcas

---
+++ catalogo/funciones/categorias.php
@@ -0,0 +1,3 @@
+<?php
+
+    #### CRUD DE categorías
---
+++ catalogo/funciones/marcas.php
@@ -0,0 +1,25 @@
+<?php
+
+    #### CRUD DE marcas
+
+    function listarMarcas()
+    {
+        $link = conectar();
+        $sql  = "SELECT idMarca, mkNombre 
+                    FROM marcas";
+        $resultado = mysqli_query($link, $sql);
+        return $resultado;
+    }
+
+    function verMarcaPorID()
+    {
+    }
+    function agregarMarca()
+    {
+    }
+    function modificarMarca()
+    {
+    }
+    function eliminarMarca()
+    {
+    }
