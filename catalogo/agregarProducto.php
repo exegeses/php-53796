c0e4bb0cc06ce4165f03e157e8fcbf681500820c
Marcos <exegeses@gmail.com>
2021-10-26 12:31:53 -0300

    Aplicación de alta de un producto

---
+++ catalogo/agregarProducto.php
@@ -0,0 +1,29 @@
+<?php
+    //require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $chequeo = agregarProducto();
+    $css = 'danger';
+    $mensaje = 'No se pudo agregar el producto.';
+    if( $chequeo ){
+        $css = 'success';
+        $mensaje = 'Producto agregado correctamente.';
+    }
+
+    include 'includes/header.html';
+    include 'includes/nav.php';
+?>
+
+    <main class="container">
+        <h1>Alta de una productos</h1>
+
+        <div class="alert alert-<?= $css ?> col-8 mx-auto">
+            <?= $mensaje ?>
+            <a href="adminProductos.php" class="btn btn-light m-2">
+                volver a panel de productos
+            </a>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
