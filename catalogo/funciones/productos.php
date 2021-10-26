b4960de931a96dcb5b21e9f7ce8db60949b1bc5f
Marcos <exegeses@gmail.com>
2021-10-26 12:14:47 -0300

    función para subir archivo

--- catalogo/funciones/productos.php
+++ catalogo/funciones/productos.php
@@ -26,3 +26,39 @@
                         or die( mysqli_error($link) );
         return $resultado;
     }
+
+    function subirImagen()
+    {
+        // si no envian archivo
+        $prdImagen = 'noDisponible.jpg';
+
+        //enviaron archivo
+        if( $_FILES['prdImagen']['error'] == 0 ){
+            /* copiamos archivo desde el /tmp a productos/ */
+            $tmp = $_FILES['prdImagen']['tmp_name']; //nombre termporal
+            $dir = 'productos/';
+            /* renombramos
+                 time()  segundos desde 1/1/1970
+                .extension */
+            $extension = pathinfo( $_FILES['prdImagen']['name'], PATHINFO_EXTENSION );
+            $prdImagen = time().'.'.$extension;
+            move_uploaded_file( $tmp, $dir.$prdImagen );
+        }
+        return $prdImagen;
+    }
+
+    function agregarProducto()
+    {
+        /*capturamos datos enviados por el form*/
+        $prdNombre = $_POST['prdNombre'];
+        $prdPrecio = $_POST['prdPrecio'];
+        $idMarca   = $_POST ['idMarca'];
+        $idCategoria   = $_POST ['idCategoria'];
+        $prdPresentacion = $_POST['prdPresentacion'];
+        $prdStock = $_POST['prdStock'];
+        /* subir archivo */
+        $prdImagen = subirImagen();
+
+        //nos conectamos...
+
+    }
