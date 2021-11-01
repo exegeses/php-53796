34a29de591871a65873c587d2f2bf8a305aa8e78
Marcos <exegeses@gmail.com>
2021-11-01 12:48:31 -0300

    función para chequear si hay productos de una marca

--- catalogo/funciones/marcas.php
+++ catalogo/funciones/marcas.php
@@ -48,6 +48,25 @@
                         or die( mysqli_error($link) );
         return $resultado;
     }
+
+    /**
+     * función para chequear si hay
+     * productos de una marca
+     * @return int $cantidad
+     *
+    */
+    function checkProductoPorMarca()
+    {
+        $idMarca = $_GET['idMarca'];
+        $link    = conectar();
+        $sql     = "SELECT 1 FROM productos
+                        WHERE idMarca = ".$idMarca;
+        $resultado = mysqli_query( $link, $sql )
+                    or die( mysqli_error($link) );
+        $cantidad = mysqli_num_rows($resultado);//cantidad de registros
+        return $cantidad;
+    }
+
     function eliminarMarca()
     {
     }
