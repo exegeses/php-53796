6831f19a0168c44f78ae0352e75c998d35beb1b3
Marcos <exegeses@gmail.com>
2021-11-02 10:31:20 -0300

    Aplicación de baja de un producto

---
+++ catalogo/eliminarProducto.php
@@ -0,0 +1,29 @@
+<?php
+    //require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $chequeo = eliminarProducto();
+    $css = 'danger';
+    $mensaje = 'No se pudo eliminar el producto.';
+    if( $chequeo ){
+        $css = 'success';
+        $mensaje = 'Producto eliminado correctamente.';
+    }
+
+    include 'includes/header.html';
+    include 'includes/nav.php';
+?>
+
+    <main class="container">
+        <h1>Baja de una marca</h1>
+
+        <div class="alert alert-<?= $css ?> col-8 mx-auto">
+            <?= $mensaje ?>
+            <a href="adminProductos.php" class="btn btn-light m-2">
+                volver a panel de productos
+            </a>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
